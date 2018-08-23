dffb63861af02447e52aa3be31ce858a0423d535
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-23 21:25:23 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,6 +7,7 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     max_rating = self.highest_rating
     Show.where("rating=?", max_rating).first
+    Show.order("rating").first
   end
 
   def self.lowest_rating
